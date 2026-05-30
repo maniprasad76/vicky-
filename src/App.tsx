4c6dab45689e965e99c44cb8cb382580056b26ac
Mani Prasad <maniprasad76@example.com>
2026-05-30 07:44:47 +0530

    remove custom cursor completely

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import ThreeBackground from './components/ThreeBackground';
-import CustomCursor from './components/CustomCursor';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
-      <CustomCursor />
       <ThreeBackground />
       <Header />
       <Routes>
